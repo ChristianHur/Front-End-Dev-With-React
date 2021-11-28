ac5ee3cdc118f5d28a2a4c42197c96d8fbdedaee
Christian Hur <chrisvhur@gmail.com>
2021-11-27 18:24:25 -0600

    Update Nav.js

--- react-router-example/src/components/Nav.js
+++ react-router-example/src/components/Nav.js
@@ -5,7 +5,8 @@ export function Nav(props) {
   return (
     <div>
       <h1>Navigation</h1>
-      <Link to="/">Home</Link> |<Link to="/flights/1">Flights/1</Link> |
+      <Link to="/">Home</Link> |
+      <Link to="/flights/1">Flights/1</Link> |
       <Link to="/edit/1">Edit/1</Link>
     </div>
   );

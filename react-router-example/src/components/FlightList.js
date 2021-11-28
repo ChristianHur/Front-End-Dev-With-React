b8da1dea6dd4ff3e34dd6766b9b93728e4d41d27
Christian Hur <chrisvhur@gmail.com>
2021-11-27 18:38:02 -0600

    Update FlightList.js

--- react-router-example/src/components/FlightList.js
+++ react-router-example/src/components/FlightList.js
@@ -3,11 +3,11 @@ import { useParams } from 'react-router-dom';
 
 export function FlightList(props) {
   let { id } = useParams(); // Unpacking and retrieve id
-  let index = props.flightData.findIndex(e => e.id === parseInt(id));
+  let index = props.flightData.findIndex((e) => e.id === parseInt(id));
   let flight = props.flightData[index];
   return (
     <div>
-      <h1>Flight Data</h1>
+      <h1>Flight List Data</h1>
       <p>Flight Number: {flight.flight_no}</p>
       <p>Airline: {flight.airline}</p>
       <p>Departure Airport: {flight.departure_airport}</p>

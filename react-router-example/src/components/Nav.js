8ca13fa4b02512d30eff4cfc0d6857e616276fe6
Christian Hur <chrisvhur@gmail.com>
2021-11-27 18:35:52 -0600

    Update Nav.js

--- react-router-example/src/components/Nav.js
+++ react-router-example/src/components/Nav.js
@@ -5,9 +5,31 @@ export function Nav(props) {
   return (
     <div>
       <h1>Navigation</h1>
-      <Link to="/">Home</Link> |
-      <Link to="/flights/1">Flights/1</Link> |
-      <Link to="/edit/1">Edit/1</Link>
+      <Link to="/">Home</Link>
+
+      <ul className="no-style-list">
+        {props.flightData.map((flight, index) => {
+          return (
+            <li key={index}>
+              <Link className="nav-link" to={'/edit/' + flight.id}>
+                Edit: {flight.flight_no}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <ul className="no-style-list">
+        {props.flightData.map((flight, index) => {
+          return (
+            <li key={index}>
+              <Link className="nav-link" to={'/flights/' + flight.id}>
+                Flight Details: {flight.flight_no}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }

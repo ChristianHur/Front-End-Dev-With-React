714ae53eb050ce22c15e2fec3d1f4676bff8d995
Christian Hur <chrisvhur@gmail.com>
2022-11-27 20:12:56 -0600

    Update AddForm.js

--- form-validations/custom/src/demo/AddForm.js
+++ form-validations/custom/src/demo/AddForm.js
@@ -1,3 +1,27 @@
+/**
+MIT License
+
+Copyright (c) 2022 Christian Hur
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+**/
+
 import React from 'react';
 
 // Child component - the Form to add one record
@@ -8,7 +32,7 @@ class AddForm extends React.Component {
         this.state = this.initState();
     }
 
-    defaultDate = '2000-01-01'
+    defaultDate = '2000-01-01';
 
     initState = () => {
         const data = { user: '', game: 'Galaga', points: 0, event: this.defaultDate };
@@ -84,7 +108,7 @@ class AddForm extends React.Component {
             validReqs.event.errPastDate = '';
         }
 
-        this.setState({ validReqs })
+        this.setState({ validReqs });
 
     }
 
@@ -95,9 +119,9 @@ class AddForm extends React.Component {
         const isValidate = (key, field = null) => {
 
             if (validReqs[key].errMsg || validReqs[key][field])
-                return 'form-control is-invalid'
+                return 'form-control is-invalid';
             else
-                return 'form-control'
+                return 'form-control';
         }
 
         return (
